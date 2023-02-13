282a0f5477c774953869acd969a708dcff4f3194
saagarsoni <saagarsoni3@gmail.com>
2023-02-13 21:48:39 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+//For Allowing Only Numeric Input 
 function validateInput(event) {
    var keyCode = event.which || event.keyCode;
    if (keyCode < 48 || keyCode > 57 && keyCode !== 8) {
      event.preventDefault();
    }
  }
+ 
  
--- php/sendEmail.php
+++ php/sendEmail.php
@@ -1,12 +1,22 @@
 <?php
-// $to_email = "saagarsoniwork@gmail.com";
-// $subject = "Simple Email Test via PHP";
-// $body = "Hi, This is test email send by PHP Script";
-// $headers = "From: saagarsoni3@gmail.com ";
+$to = "saagarsoniwork@gmail.com";
+$subject = "New Contact Enquiry";
+$headers = "From: Saagar Soni" ;
 
-if (mail($to_email, $subject, $body, $headers)) {
-    echo "Email successfully sent to $to_email...";
-} else {
-    echo "Email sending failed...";
-}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$phone = $_POST['phone'];
+
+$message = "From: $name \r\n".
+                   "Email Id : $email \r\n".
+                   "Phone No : $phone \r\n".
+                   "Message : $message \r\n";
+
+echo $_POST['name'];
+echo $_POST['email'];
+echo $_POST["phone"];
+echo $_POST["message"];
+
+ mail($to, $subject, $message);
 ?>
