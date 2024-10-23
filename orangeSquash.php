4c3c0d1b48703a2ac9e241a2d39a080bf77a7eee
saagarsoni <saagarsoni@hotmail.com>
2024-10-23 20:52:42 +0530

    ok

--- orangeSquash.php
+++ orangeSquash.php
@@ -7,45 +7,78 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
 <form action="manage_cart.php" method="post" class="product p-2">
    
-   <!-- Product Description Page -->
-   <div class="container">
-      <div class="row">
-         <div class="col-lg-6">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+<!-- Product Description Page -->
+<main class="container py-5">
+  <div class="row">
+    <div class="col-lg-6">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="./images/orangeSquash1.jpg" class=" w-100 img-fluid" alt="Lemon Squash"style="height: 400px;">
-            </div>
-            <div class="carousel-item">
-                <img src="./images/orangeSquash2.jpg" class=" w-100 img-fluid" alt="Lemon Squash"  style="height: 450px;">
-            </div>
-            <!-- <div class="carousel-item">
-                <img src="./images/lemonSquash3.jpg" class="d-block w-100" alt="Lemon Squash">
-            </div> -->
-            <!-- Add more carousel items as needed -->
+          <div class="carousel-item active">
+            <img src="./images/orangeSquash1.jpg" alt="Orange Squash" class="w-100 img-fluid">
+          </div>
+          <div class="carousel-item">
+            <img src="./images/orangeSquash2.jpg" alt="Orange Squash" class="w-100 img-fluid">
+          </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
         </button>
-    </div>
-    <p class="card-text"><b> <span>&#8377;</span> 180 for 750 ml</b></p>
-    <button type="submit" name="addToCart" style="width: 100%;" class="btn bg-success text-light fw-bold">Add To Cart <i
-            class="fas fa-shopping-cart"></i></button>
-    <input type="hidden" name="item_name" value="orangeSquash">
-    <input type="hidden" name="price" value="1">
-</div>
-
-         <div class="col-lg-6">
-            <div class="w-100 fs-6 bg-warning p-1 fw-bold fs-4 rounded text-center">Orange Squash</div>
-               <p id="orangeSquashDetail" class="text-sm-start lh-sm"></p>
-         </div>
       </div>
-   </div>
+      <p class="card-text"><b><span>&#8377;</span> 180 for 750 ml</b></p>
+      <form action="manage_cart.php" method="post">
+        <button type="submit" name="addToCart" class="btn bg-success text-light fw-bold w-100">
+          Add To Cart <i class="fas fa-shopping-cart"></i>
+        </button>
+        <input type="hidden" name="item_name" value="orangeSquash">
+        <input type="hidden" name="price" value="1">
+      </form>
+    </div>
+    <div class="col-lg-6">
+      <div class="w-100 fs-6 bg-warning p-1 fw-bold fs-4 rounded text-center">
+        Orange Squash
+      </div>
+      <p id="orangeSquashDetail" class="text-sm-start lh-sm"></p>
+    </div>
+  </div>
+</main>
+
+<style>
+
+/* Product Page Styles */
+main {
+  font-family: Arial, sans-serif;
+}
+
+.carousel-inner img {
+  height: 400px;
+  object-fit: cover;
+}
+
+.card-text {
+  font-size: 1.5rem;
+  color: #333;
+}
+
+.bg-success:hover {
+  background-color: #2ecc71;
+}
+
+/* Mobile Responsiveness */
+@media (max-width: 768px) {
+  .carousel-inner img {
+    height: 250px;
+  }
+  .col-lg-6 {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+}
+</style>
 </form>
 <script>
    //Orange Squash
